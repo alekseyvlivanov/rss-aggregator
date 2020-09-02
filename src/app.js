0b2099317a54dd00fd2be8c2f43123e91474521d
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-09-03 00:06:40 +1000

    feat: add basic url fetch with cors proxy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-// import axios from 'axios';
+import axios from 'axios';
 import { object, string } from 'yup';
 
 import { elements, initView } from './view';
 
-// const parseUrl = (url) => {
-//   console.log(url);
-// };
+const parseUrl = (url) => {
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  axios
+    .get(`${proxy}${url}`)
+    .then((response) => console.log(response))
+    .catch((err) => console.log(err));
+};
 
 const langs = ['en', 'ru'];
 
@@ -36,6 +40,8 @@ const app = () => {
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (watched.form.url !== '') {
+      parseUrl(watched.form.url);
+
       watched.feeds.push(watched.form.url);
       watched.form.url = '';
       elements.form.reset();
